7381f2bd6cdcf7629267cc1907efa7651eac2d64
Stayman Hou <stayman.hou@gmail.com>
2015-05-20 17:05:14 -0700

    update for prod

--- scripts/Fabfile.py
+++ scripts/Fabfile.py
@@ -1,7 +1,7 @@
 from __future__ import with_statement
 from fabric.api import *
 
-HOSTS = ['172.31.16.102']
+HOSTS = ['172.31.16.102', '172.31.16.103']
 USER = 'ubuntu'
 
 
@@ -11,4 +11,4 @@ def environment():
 
 
 def deploy():
-    run("cd /var/www/html && git pull origin master && php composer.phar install && php vendor/bin/phinx migrate -e production")
+    run("cd /var/www/html/sample_app && git pull origin master && php composer.phar install && php vendor/bin/phinx migrate -e production")
